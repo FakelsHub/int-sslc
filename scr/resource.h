eb8549e116afab1fe46a5087afab1606bc2651ce
Mr.Stalin <fakels@yandex.ru>
2019-07-31 18:52:14 +0300

    Добавление ресурсных файлов для версии.

---
+++ scr/resource.h
@@ -0,0 +1,13 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by sslc.rc
+
+#define VERSION_C  "4.1.5.0"
+#define VERSION_S  4,1,5,0
+
+#ifdef BUILDING_DLL
+#define FILE_S  "parser.dll"
+#else
+#define FILE_S  "compile.exe"
+#endif
+
