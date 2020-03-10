17637f03826e4d0824b35b8bc9a1e74ecfe6e55a
Mr.Stalin <Fakels@yandex.ru>
2020-03-10 17:49:14 +0300

    Fixed crash for parser.dll Added Debug configuration for DLL build.

--- sslc/resource.h
+++ sslc/resource.h
@@ -1,9 +1,8 @@
-//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by sslc.rc
 
-#define VERSION_C  "1.2.0.0"
-#define VERSION_S  1,2,0,0
+#define VERSION_C  "1.2.1.0"
+#define VERSION_S  1,2,1,0
 
 #ifdef BUILDING_DLL
 #define FILE_S  "parser.dll"
