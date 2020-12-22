e526c7dc61c30e8cacdc295c0793946286bd9a8d
Mr.Stalin <fakels@yandex.ru>
2020-12-22 03:53:46 +0300

    SSLC: Added new opcodes sfall_func7 and sfall_func8. Added skipping of the BOM marker for scripts in UTF-8 encoding.

--- sslc/resource.h
+++ sslc/resource.h
@@ -1,8 +1,11 @@
 // Microsoft Visual C++ generated include file.
 // Used by sslc.rc
 
-#define VERSION_C  "1.2.2.1"
-#define VERSION_S  1,2,2,1
+#define VERSION_C          1,2,3,0
+#define VERSION_S         "1.2.3.0"
+
+#define PRODUCT_VERSION    4,2,9,0
+#define PRODUCT_VERSION_S "4.2.9"
 
 #ifdef BUILDING_DLL
 #define FILE_S  "parser.dll"
